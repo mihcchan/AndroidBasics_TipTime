03fb8fa43d8819507cfab07f815373c1d16fce8f
Mihcchan <48868786+mihcchan@users.noreply.github.com>
2021-03-18 12:23:16 -0300

    calculate tip function added

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    //lateinit is a promise that your code will initialize the variable before using it. If you don't, your app will crash.
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    fun calculateTip(){
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId){
+            R.id.option_15_percent -> 0.15
+            R.id.option_18_percent -> 0.18
+            else -> 0.20
+        }
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpTipSwitch.isChecked
+        if (roundUp){
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount_string, formattedTip)
     }
 }
